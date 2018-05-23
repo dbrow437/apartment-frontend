a2cb6093ab6f0977e5b035952241130919505f1e
learn <learn@learn10.local>
2018-05-23 09:12:40 -0700

    Update to Auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import '../css/Login.css';
+import AuthService from '../services/AuthService';
 
 class Login extends Component {
   constructor(){
     super()
+    this.Auth = new AuthService()
     this.state={
       email: '',
       password: ''
@@ -14,12 +16,21 @@ class Login extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
+  handleFormSubmit(e){
+      e.preventDefault()
+      this.Auth.login(this.state.email,this.state.password)
+      .then(res =>{
+        this.props.history.replace('/')
+      })
+      .catch(err =>{ alert(err) })
+    }
+
   render() {
     return (
       <div className="center">
         <div className="card">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={this.handleFormSubmit.bind(this)}>
             <input
               className="form-item"
               placeholder="email goes here..."
