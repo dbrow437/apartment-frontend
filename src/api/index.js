d9640d4240c87d0476e12dbd118e7950bc74a4dc
learn <learn@learn10.localdomain>
2018-05-25 14:32:45 -0700

    url change

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 // the address of our rails backend, saved as a constant value, because we never want to accidently change it
-const BASE = 'http://localhost:3000'
+const BASE = 'https://crappy-apts.herokuapp.com'
+//const BASE = 'http://localhost:3000'
 
 let getApartments = function() {
     // the function name getCats is intended to remind you of the restful rails route --> GET '/cats'.
